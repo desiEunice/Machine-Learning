8e78b92593e3d1d94925fffa10f20a3e2ea38e00
Eunice Ofori-Addo <64563980+desiEunice@users.noreply.github.com>
2022-03-28 15:58:00 -0700

    Create utils.py
    z transform function: 
    This `z_transform(X, degree = 2)` function takes and transforms an $n \times d$ matrix, which represents the $n$ samples from the $X$ space where each sample has $d$ features, into another matrix $Z$ of dimension size $n \times d'$ that represents the transformed samples in the $Z$ space, where each sample had $d'$ features.

---
+++ utils.py
@@ -0,0 +1,69 @@
+# Implementation of functions for data manipulation for machine learning programs. 
+# Author: Eunice Ofori-Addo
+
+
+# Various tools for data manipulation. 
+# Z transformation function. Support the pocket version for linearly unseparatable data. 
+
+
+
+import numpy as np
+import math
+
+def main():
+    X = np.array([[1,2], [2,3],[3,4]])
+    Z = MyUtils.z_transform(X,2)
+    print(Z)
+
+class MyUtils:
+
+    
+    def z_transform(X, degree = 2):
+        ''' Transforming traing samples to the Z space
+            X: n x d matrix of samples, excluding the x_0 = 1 feature
+            degree: the degree of the Z space
+            return: the n x d' matrix of samples in the Z space, excluding the z_0 = 1 feature.
+            It can be mathematically calculated: d' = \sum_{k=1}^{degree} (k+d-1) \choose (d-1)
+        '''
+        
+        
+        
+        r = degree
+        if r == 1:
+            return X
+
+        Z = X.copy()
+        
+        #r = degree
+        n,d = X.shape
+        B = []
+        
+        for i in range(r):
+            B.append(math.comb(d+i, d-1))
+            
+        l = np.arange(np.sum(B))
+            
+        q = 0
+        p = d
+        g = d
+        
+      
+        
+        for i in range(1,r):
+            for j in range(q, p):
+                
+                head = l[j]
+                for k in range(head, d):
+                    temp = (Z[:,j] * X[:,k]).reshape(-1,1)
+                    Z = np.append(Z, temp, axis = 1)
+                    l[g] = k
+                    g = g + 1
+                    
+            q = p 
+            p = p + B[i]
+                
+        return Z
+        
+        
+if __name__ == '__main__':
+    main()        
